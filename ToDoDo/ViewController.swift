030a522e5a234bb801eae519dc036025c81cc0ac
datsnet <datsumi1013@gmail.com>
2015-02-13 00:39:46 +0900

    Initial Commit

---
+++ ToDoDo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDoDo
+//
+//  Created by 熱海 大介 on 2015/02/13.
+//  Copyright (c) 2015年 com.datsnet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
